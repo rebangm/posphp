92d53f39d7e183f111a873795df9687349cc2f9e
rebangm <jp.depigny@gmail.com>
2013-06-20 18:12:26 +0200

    Update userManage.js

--- src/Pos/UserBundle/Resources/public/js/userManage.js
+++ src/Pos/UserBundle/Resources/public/js/userManage.js
@@ -4,19 +4,23 @@
  */
 
 $(document).ready(function() {
+    $("#usertable button").on('click',function(event){
+        userSetActive($(this).attr("data-url"));
+    });
+});
 
 
-    function userSetActive(url){
+function userSetActive(url,success,error){
         $.ajax({
             type: 'GET', // Le type de ma requete
             url: url, // L'url vers laquelle la requete sera envoyee
             success: function(data, textStatus, jqXHR) {
+                success();
                 alert(data);
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 alert(jqXHR);
             }
         });
-    }
+}
 
-})
