10f2a0cf1b4fcb5f955d611405cd66e9235ce923
Jean Philippe Depigny <jpdepigny@jpdepigny-vdi.vm.vdi.s1.p.fti.net>
2013-06-21 16:23:18 +0200

    add js for user active control

--- src/Pos/UserBundle/Resources/public/js/userManage.js
+++ src/Pos/UserBundle/Resources/public/js/userManage.js
@@ -15,13 +15,16 @@ function userSetActive(button){
             type: 'GET', // Le type de ma requete
             url: button.attr("data-url"), // L'url vers laquelle la requete sera envoyee
             success: function(data, textStatus, jqXHR) {
-                //button
-                
-                if (data.message.active == 1) 
+                console.log($("i",button));
+				
+                if (data.message.active == 1){ 
                     button.addClass("btn-success").removeClass("btn-danger");
-                else if(data.message.active == 0)
+					$("i",button).addClass("icon-ok").removeClass("icon-remove");
+				}
+                else if(data.message.active == 0){
                     button.addClass("btn-danger").removeClass("btn-success");
-                
+					$("i",button).addClass("icon-remove").removeClass("icon-ok");
+				}
                 button.attr("data-url", data.message.url);
             },
             error: function(jqXHR, textStatus, errorThrown) {
