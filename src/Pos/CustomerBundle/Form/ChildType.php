14a7cb520aecb7de4177b547e69d81b2075f4592
rebangm <jp.depigny@gmail.com>
2013-10-31 17:02:32 +0100

    add new form and css

---
+++ src/Pos/CustomerBundle/Form/ChildType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pos\CustomerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ChildType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('birthDate')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Pos\CustomerBundle\Entity\Child'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pos_customerbundle_child';
+    }
+}
