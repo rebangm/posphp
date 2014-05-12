8378ea7fb369a6365e09d39bf4aeeb12a9b34ff2
Rebangm <jp.depigny@gmail.com>
2014-05-12 22:05:44 +0200

    add index action and view for product listing

--- src/Pos/ProductBundle/Controller/ManageController.php
+++ src/Pos/ProductBundle/Controller/ManageController.php
@@ -10,7 +10,41 @@ class ManageController extends Controller
 {
     public function indexAction($page)
     {
-        return $this->render('PosProductBundle:Manage:index.html.twig', array('page' => $page));
+        if ( $page < 1 ) {
+            $error = "the page requested doesn't exist";
+            $this->get('session')->getFlashBag()->add('error', $error);
+            return $this->redirect($this->generateUrl('pos_customer_manage',
+                array( 'page' => 1 )));
+        }
+
+        $limit  = 5;
+
+        $em = $this->getDoctrine()->getManager();
+        //$dql   = "SELECT a FROM PosUserBundle:User a ORDER BY a." . $order . " ASC";
+        $dql   = "SELECT c FROM PosProductBundle:Product c ORDER BY c.id ASC";
+
+        $query = $em->createQuery($dql);
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $query,
+            $page,
+            $limit
+        );
+
+        $pagination->setTemplate('PosPaginatorBundle::slidingPagination.html.twig');
+        $pagination->setUsedRoute('pos_customer_manage_list');
+
+        return $this->render('PosProductBundle:Manage:manage.html.twig',
+            array('pagination' => $pagination ));
+
+    }
+
+    /**
+     * @param Customer $customer
+     * @ParamConverter("customer", options={"mapping": {"customer_id": "id"}})
+     */
+    public function editAction(Product $product){
+
     }
 
     /**
