ec4c1d49268744f9ac08775aba107c013ab129d6
Rebangm <jp.depigny@gmail.com>
2014-05-17 23:10:23 +0200

    add toString method

--- src/Pos/ProductBundle/Entity/Vat.php
+++ src/Pos/ProductBundle/Entity/Vat.php
@@ -28,6 +28,14 @@ class Vat
      */
     private $rate;
 
+
+    /**
+     *
+     */
+    public function __toString(){
+        return $this->rate . " %";
+    }
+
     /**
      * Get id
      *
