73e17447339be7324db945c3fbf4f035b2faab6c
Rebangm <jp.depigny@gmail.com>
2014-05-17 23:35:14 +0200

    install avalanche and barcode bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Avalanche\Bundle\ImagineBundle\AvalancheImagineBundle(),
+            new Mopa\Bundle\BarcodeBundle\MopaBarcodeBundle(),
             new Pos\HRBundle\PosHRBundle(),
             new Pos\UserBundle\PosUserBundle(),
             new Pos\HomeBundle\PosHomeBundle(),
