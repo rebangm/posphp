a9bafcccd99776136c6b260a24217e7ad78bec3f
Jean-philippe Depigny <jdepigny.ext@orange.com>
2014-09-24 17:31:31 +0200

    add saleSession entity, add session template, add routing for saleSesion

--- src/Pos/PointOfSalesBundle/Controller/OutletStoreController.php
+++ src/Pos/PointOfSalesBundle/Controller/OutletStoreController.php
@@ -2,14 +2,20 @@
 
 namespace Pos\PointOfsalesBundle\Controller;
 
+use MyProject\Proxies\__CG__\OtherProject\Proxies\__CG__\stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class OutletStoreController extends Controller
 {
     public function indexAction()
     {
+        $saleSession = false;
+        //$session = new saleSession();
+        //if($session->isOpen()){
 
 
-        return $this->render('PosPointOfSalesBundle:OutletStore:index.html.twig');
+
+        return $this->render('PosPointOfSalesBundle:OutletStore:index.html.twig',
+            array('saleSession' => $saleSession ));
     }
 }
---
+++ src/Pos/PointOfSalesBundle/Entity/SaleSession.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Pos\PointOfSalesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * SaleSession
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Pos\PointOfSalesBundle\Entity\SaleSessionRepository")
+ */
+class SaleSession
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="startDate", type="datetime")
+     */
+    private $startDate;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="endDate", type="datetime")
+     */
+    private $endDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set startDate
+     *
+     * @param \DateTime $startDate
+     * @return SaleSession
+     */
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+    
+        return $this;
+    }
+
+    /**
+     * Get startDate
+     *
+     * @return \DateTime 
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * Set endDate
+     *
+     * @param \DateTime $endDate
+     * @return SaleSession
+     */
+    public function setEndDate($endDate)
+    {
+        $this->endDate = $endDate;
+    
+        return $this;
+    }
+
+    /**
+     * Get endDate
+     *
+     * @return \DateTime 
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+}
